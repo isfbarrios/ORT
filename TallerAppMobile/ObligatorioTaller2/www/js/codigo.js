038aeac72032e3706a1c7e137802d5c3b8e6f093
isfbarrios <fabribarrios@hotmail.com>
2023-02-19 13:54:50 -0300

    Avances en obligatorio de taller de mobile apps.

--- TallerAppMobile/ObligatorioTaller2/www/js/codigo.js
+++ TallerAppMobile/ObligatorioTaller2/www/js/codigo.js
@@ -2,14 +2,10 @@ const BaseURL = 'https://dwallet.develotion.com';
 const BaseImgURL = `${BaseURL}/imgs`;
 const menu = document.querySelector('#menu');
 const menu_ruteo = document.querySelector('#menu-ruteo');
-const menuItems = document.querySelectorAll(".menu-item");
-let token = '';
-let departamentos = '';
-let ciudades = '';
-let btnLogin = document.querySelector('#btnLogin').addEventListener('click', login);
-const btnDepartamentos = document.querySelector("#idDepartamento");
+let map = null;
+let ubicacionActual;
 
-document.querySelector('#idDepartamento').addEventListener('ionChange', filtrarCiudades);
+inicializar();
 
 function filtrarCiudades() {
     document.querySelector("#idCiudad").innerHTML = "";
@@ -28,79 +24,45 @@ function filtrarCiudades() {
     })
 }
 
-
-inicializar();
-
 function inicializar() {
     menu_ruteo.addEventListener('ionRouteWillChange', navegar);
-    menu_ruteo.push("/");
-    document.querySelector("#btnLogin").addEventListener("click",Login);
-    document.querySelector("#btnRegistroUsuario").addEventListener("click",Registro);
+    
+    document.querySelector("#btnLogin").addEventListener("click", Login);
+    //document.querySelector("#btnCerrarSesion").addEventListener("click", cerrarSesion);
+    document.querySelector("#btnRegistroUsuario").addEventListener("click", Registro);
+    document.querySelector('#btnFiltrarCajeros').addEventListener("click", localizarCajeros);
+    document.querySelector('#idDepartamento').addEventListener('ionChange', filtrarCiudades);
+    document.querySelector('#btnAgregarGasto').addEventListener('click', agregarGasto);
+    document.querySelector('#btnTipoTransferencia').addEventListener('click', tipoTransferencia);
 
-    //Disparo consultas asincronas
+    navigator.geolocation.getCurrentPosition(devolverPosicion, devolverError);
+
+    //Disparo consultas asincronas solo si no tengo los datos almacenados.
     precargaDepartamentos();
-    precargaCiudades();
-
+    precargaCajeros();
+    
     verificarSiEstaLogueado();
 }
 
-function navegar(event) {
-
-    //Concateno el atributo url del elemento ion-route clickeado (le quito la "/"). Ej: url="/login" => "login".
-    let page = `#page-${String(event.detail.to).substring(1)}`;
-    //Corroboro si era la Home, dado que esta maneja como URL únicamente "/".
-    if (event.detail.to === '/') page = '#page-home';
-    //Oculto todas las secciones previo a mostrar la correspondinete.
-    ocultarSecciones();
-    document.querySelector(page).style.display = 'block';
-
-    if (page === '#page-productos') listadoDeProductos();
-}
-
-function ocultarSecciones() {
-    document.querySelectorAll('ion-page[id*="page-"]').forEach((page) => {
-        page.style.display = 'none';
-    });
-}
-
-function cerrarMenu() {
-    menu.close();
-}
-
 function verificarSiEstaLogueado() {
-
-    if(localStorage.getItem("token") !== null || localStorage.getItem("token") !== "") {
-
-        menu_ruteo.push("/");
-
+    if(localStorage.getItem("token") !== undefined && localStorage.getItem("token")) {
+        precargaRubros();
+        precargaMovimientos();
+        menuRuteo('/');
+        document.querySelectorAll('.menu-log-in').forEach((el) => el.style.display = 'none');
+        document.querySelector('.menu-log-out').style.display = 'block';
     }
-    //TODO: Ver este clear().
-    //token.clear();
+    else {
+        document.querySelectorAll('.menu-log-in').forEach((el) => el.style.display = 'block');
+        document.querySelector('.menu-log-out').style.display = 'none';
+        menuRuteo("/login");
+        
+    }
 }
 
-function listadoDeProductos() {
-    if (localStorage.getItem("token") != null && localStorage.getItem("token") != "") {
-
-        let productList = document.querySelector('#product-list');
-
-        fetch(`${BaseURL}/api/productos`,{
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-                "x-auth": localStorage.getItem('token')
-            }
-        })
-        .then((response) => response.json())
-        .then((datosProductos) => datosProductos.data.forEach(producto => {
-            console.log(producto);
-            productList.innerHTML += 
-                `<ion-item class="${producto._id}" class="${producto.codigo}">
-                <img src="${BaseImgURL}/${producto.urlImagen}" class="img-responsive"/>
-                <ion-label>${producto.nombre}</ion-label>
-                </ion-item>`;
-        }))
-        .catch((error) => console.error('Error', error));
-    }
+function localizarCajeros() {
+    let distancia = parseInt(document.querySelector('#distanciaCajero').value);
+    if (distancia > 0) getCajeros(distancia);
 }
 
 function Login() {
@@ -112,7 +74,7 @@ function Login() {
             throw new Error("El nombre de usuario no puede estar vacia");
         }
         if (password === "") {
-            throw new Error("La password no puede estar vacio");
+            throw new Error("La password no puede estar vacia");
         }
 
         fetch(`${BaseURL}/login.php`, 
@@ -123,15 +85,17 @@ function Login() {
             },
             body: JSON.stringify({
                 "usuario": email,
-                "password": password
+                "password": password,
+            
             })
         })
         .then((response) => response.json())
         .then((data) => {
             //Guardo el token en el localStorage
-            token = data.apiKey.trim();
-            localStorage.setItem('token', token);
-            menu_ruteo.push('/');
+            localStorage.setItem('token', data.apiKey);
+            localStorage.setItem('id', data.id);
+
+            verificarSiEstaLogueado();
         })
         .catch((error) => console.error('Error', error));
     }
@@ -147,25 +111,20 @@ function Registro() {
     let departamento = document.querySelector("#idDepartamento").value.trim();
     let ciudad = document.querySelector("#idCiudad").value.trim();
 
-    //Sanitizo departamento y ciudad. Los dejo en camelCase
-    departamento = departamento.substring(0,1).toUpperCase() + departamento.substring(1).toLowerCase();
-    ciudad = ciudad.substring(0,1).toUpperCase() + ciudad.substring(1).toLowerCase();
-
     document.querySelector("#mensajeRegistro").innerHTML = "";
 
     try {
-        if (nombreUsuario === "")throw new Error("El nombre no puede estar vacio");
+        if (nombreUsuario === "") throw new Error("El nombre no puede estar vacio");
 
         if (password === "")  throw new Error("La contraseña no puede estar vacia");
 
         if (password.length < 8)  throw new Error("La contraseña debe tener 8 caracteres como mínimo");
 
-        if (password != repeatePassword) throw new Error("Las contraseñas deben coincidir");
+        if (password !== repeatePassword) throw new Error("Las contraseñas deben coincidir");
 
         if (departamento === "") throw new Error("El departamento es obligatorio");
 
         if (ciudad === "") throw new Error("La direccción es bligatoria");
-
 
         fetch(`${BaseURL}/usuarios.php`, 
         {
@@ -176,12 +135,14 @@ function Registro() {
             body:JSON.stringify({
                 usuario: nombreUsuario,
                 password: password,
-                idDepartamento: 3218,
-                idCiudad: 129833,
+                idDepartamento: departamento,
+                idCiudad: ciudad,
             })
         })
         .then((response) => {
             if(response.ok) {
+                document.querySelector("#mensajeRegistro").innerHTML = "Registro exitoso";
+                verificarSiEstaLogueado();
                 return response.json();
             }
             else {
@@ -189,24 +150,186 @@ function Registro() {
                 throw new Error("Ya existe usuario.");
             }
         })
-        .then((datos) => {
-            document.querySelector("#mensajeRegistro").innerHTML = "Registro exitoso"
-        })
         .catch((data) => {
             document.querySelector("#mensajeRegistro").innerHTML = data.message
         });
-
-        limpiarCampos();
-        
     }
     catch (Error) {
         document.querySelector("#mensajeRegistro").innerHTML = Error.message;
     }
 }
+/* Movimientos INI */
+function tipoTransferencia() {
+    let tipo = document.querySelector("#tipoTransferencia").value.trim();
+    //Si tipo viene vacio, le setteo ingreso por defecto, sino, lo dejo como estaba.
+    tipo = tipo === '' ? 'ingreso' : tipo;
+    //Filtro los rubros según el tipo.
+    obtenerRubroDelGasto(tipo);
+    menuRuteo('/gastos');
+}
 
-function CerrarSesion() {
-    localStorage.clear();
-    localStorage.removeItem("token");
+function obtenerRubroDelGasto(tipo) {
+    document.querySelector("#idRubros").innerHTML ="";
+    let rubrosJSON = JSON.parse(localStorage.getItem('rubros'));
+
+    if (rubrosJSON !== null) {
+        rubrosJSON.forEach((rubro) => {
+            if(rubro.tipo === tipo) {
+                document.querySelector("#idRubros").innerHTML +=`<ion-select-option value="${rubro.id}">${rubro.nombre}</ion-select-option>`;
+            }
+        });
+    }
+}
+
+function agregarGasto() {
+    let concepto = document.querySelector("#idConcepto").value.trim();
+    let total = document.querySelector("#precio").value.trim();
+    let medio = document.querySelector("#selPago").value.trim();
+    let fecha = document.querySelector("#fecha").value.trim();
+    let rubro = parseInt(document.querySelector("#idRubros").value);
+
+    console.log(`concepto ${concepto} total ${total} medio ${medio} fecha ${fecha} rubro ${rubro}`);
+
+    fetch(`${BaseURL}/movimientos.php`,
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "apikey": localStorage.getItem("token")
+        },
+        body: JSON.stringify({
+            "idUsuario": localStorage.getItem("id"),
+            "concepto": concepto,
+            "categoria": rubro,
+            "total": total,
+            "medio": medio,
+            "fecha": fecha
+        })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        localStorage.setItem('movimientos', data.movimientos);
+    })
+    .catch((error) => console.error('Error', error));
+
+    document.querySelector("#mensajeGasto").innerHTML = "Gasto guardado";
+}
+
+function mostrarMovimientos() {
+    let movimientosJSON = JSON.parse(localStorage.getItem('movimientos'));
+
+    //Si tengo el listado de rubros continuo.
+    if (movimientosJSON !== null) {
+
+        let html = '';
+
+        movimientosJSON.forEach((mov) => {
+
+            html += 
+            `<ion-col>
+                <ion-card id="movimiento-${mov.id}">
+                    <img src="${BaseImgURL}/${mov.imagen}" />
+                    <ion-card-header>
+                        <ion-card-subtitle>${mov.id}</ion-card-subtitle>
+                        <ion-card-title>${mov.nombre}</ion-card-title>
+                    </ion-card-header>
+                    <ion-card-content>
+                        Tipo: ${mov.tipo}
+                    </ion-card-content>
+                </ion-card>
+            </ion-col>`;
+        });
+
+        document.querySelector('#rubros .row').innerHTML = html;
+    }
+}
+/* Movimientos END */
+
+/* Cajeros INI */
+function getCajeros(distancia) {
+    let cajerosJSON = JSON.parse(localStorage.getItem('cajeros'));
+    let latLongActual = L.latLng(ubicacionActual[0], ubicacionActual[1]);
+
+    //Solo lo inicializo si es la primera vez.
+    if (map !== null) {
+        map.remove();
+        map = null;
+    }
+    if (map === null) {
+        map = L.map('map').setView([ubicacionActual[0], ubicacionActual[1]], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    }
+    //Si tengo el listado de cajeros continuo.
+    if (cajerosJSON !== null) {
+        cajerosJSON.forEach((cajero) => {
+            let cajeroCoordenadas = L.latLng(cajero.latitud, cajero.longitud);
+
+            if (map.distance(latLongActual, cajeroCoordenadas) <= distancia) {
+                L.marker(cajeroCoordenadas).addTo(map)
+                .bindPopup(`<input type='button' onclick=MostrarCoordenadas('${latLongActual.lat}','${latLongActual.long}')>`);
+            }
+        });
+    }
+    menuRuteo('/cajeros');
+}
+
+function devolverPosicion(pos) {
+    ubicacionActual = [pos.coords.latitude, pos.coords.longitude];
+}
+
+function devolverError(err) {
+    console.log(`ERROR(${err.code}): ${err.message}`);
+}
+/* Cajeros END */
+
+function precargaMovimientos() {
+    fetch(`${BaseURL}/movimientos.php?idUsuario=${localStorage.getItem('id')}`,
+    {
+        method:"GET",
+        headers: {
+            "Content-Type": "application/json",
+            "apikey": localStorage.getItem("token")
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        //Guardo el objeto en el localStorage
+        localStorage.setItem('movimientos', JSON.stringify(data.movimientos));
+
+        for(let i = 0; i < data.movimientos.length; i++) {
+            document.querySelector("#idRubro").innerHTML +=`<ion-select-option value="${data.movimientos[i].id}">${data.movimientos[i].nombre}</ion-select-option>`;
+        }
+    })
+    .catch((error) => console.error('Error', error));
+}
+function precargaRubros() {
+    fetch(`${BaseURL}/rubros.php`, 
+    {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'apiKey': localStorage.getItem('token')
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        //Guardo el objeto en el localStorage
+        localStorage.setItem('rubros', JSON.stringify(data.rubros));
+    })
+    .catch((error) => console.error('Error', error));
+}
+
+function precargaCajeros() {
+    fetch(`${BaseURL}/cajeros.php`)
+    .then((response) => response.json())
+    .then((data) => {
+        //Guardo el objeto en el localStorage
+        localStorage.setItem('cajeros', JSON.stringify(data.cajeros));
+    })
+    .catch((error) => console.error('Error', error));
 }
 
 function precargaDepartamentos() {
@@ -214,13 +337,13 @@ function precargaDepartamentos() {
     .then(response => response.json())
     .then(data => {
         //Guardo el objeto en el localStorage
-        departamentos = data.departamentos;
-        localStorage.setItem('departamentos', JSON.stringify(departamentos));
+        localStorage.setItem('departamentos', JSON.stringify(data.departamentos));
 
-        for(let i = 0; i < data.departamentos.length;i++) {
+        for(let i = 0; i < data.departamentos.length; i++) {
             document.querySelector("#idDepartamento").innerHTML +=`<ion-select-option value="${data.departamentos[i].id}">${data.departamentos[i].nombre}</ion-select-option>`;
         }
     })
+    .then(() => precargaCiudades())
     .catch((error) => console.error('Error', error));
 }
 
@@ -229,9 +352,44 @@ function precargaCiudades() {
     .then(response => response.json())
     .then(data => {
         //Guardo el objeto en el localStorage
-        ciudades = data.ciudades;
-        localStorage.setItem('ciudades', JSON.stringify(ciudades));
+        localStorage.setItem('ciudades', JSON.stringify(data.ciudades));
     })
     .catch((error) => console.error('Error', error));
+}
 
+function navegar(event) {
+    //Concateno el atributo url del elemento ion-route clickeado (le quito la "/"). Ej: url="/login" => "login".
+    let page = `#page-${String(event.detail.to).substring(1)}`;
+    //Corroboro si era la Home, dado que esta maneja como URL únicamente "/".
+    if (event.detail.to === '/') page = '#page-home';
+    //Oculto todas las secciones previo a mostrar la correspondinete.
+    ocultarSecciones();
+    document.querySelector(page).style.display = 'block';
+}
+
+function ocultarSecciones() {
+    document.querySelectorAll('ion-page[id*="page-"]').forEach((page) => {
+        page.style.display = 'none';
+    });
+}
+
+function cerrarMenu() {
+    menu.close();
+}
+
+function menuRuteo(url) {
+    //Limpio todos los campos cada vez que se cambia la ruta.
+    document.querySelectorAll('.field').forEach((field) => {
+        field.value = '';
+    });
+    url = url === '' ? '/' : url;
+    menu_ruteo.push(url);
+}
+
+function cerrarSesion() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("id");
+    localStorage.removeItem("movimientos");
+    cerrarMenu();
+    inicializar();
 }
